24021228267a69ed89f5747e3b55dc224ead9a66
Heallers <thomasbourgoin@hotmail.fr>
2021-12-17 23:46:31 +0100

    Feature 2
    On ne passe plus par une base SQL pour faire du 100% python

--- api.py
+++ api.py
@@ -9,6 +9,7 @@ app = FastAPI()
 m_host = "localhost"
 m_port = 8080
 
+
 @app.get("/annotations")
 async def read_annotations():
     return database.get_annotations()
@@ -22,6 +23,11 @@ async def read_number_annotations_by_user():
 @app.get("/number_annotations_by_project")
 async def read_number_annotations_by_project():
     return database.get_number_annotations_by_project()
+
+
+@app.get("/assets_with_majority_different_labels")
+async def read_assets_with_majority_different_labels(_project_id: str):
+    return database.get_assets_with_majority_different_labels(_project_id)
 
 
 def run():
--- database.py
+++ database.py
@@ -1,125 +1,61 @@
-import sqlite3
-import datetime
 import jsonManager
-
-l_conn = sqlite3.connect(':memory:', check_same_thread=False)
-
-
-def create_tables():
-    l_cursor = l_conn.cursor()
-
-    l_cursor.execute("create table users (id INTEGER PRIMARY KEY NOT NULL, user TEXT)")
-
-    l_cursor.execute("create table projects (id INTEGER PRIMARY KEY NOT NULL, project_id TEXT)")
-
-    l_cursor.execute("create table annotations ("
-                     "id INTEGER PRIMARY KEY NOT NULL, "
-                     "user VARCHAR(100), "
-                     "project_id VARCHAR(100), "
-                     "asset_id VARCHAR(100), "
-                     "label VARCHAR(100), "
-                     "is_last_checkpoint BIT, "
-                     "date TIMESTAMP, "
-                     "FOREIGN KEY(user) REFERENCES users(user), "
-                     "FOREIGN KEY(project_id) REFERENCES projects(project_id))")
-
-    l_cursor.close()
-
-
-def insert_data():
-    l_annotations = jsonManager.parse_annotations()
-    l_users_set = set()
-    l_projects_set = set()
-    l_annotations_list = []
-
-    for l_annotation in l_annotations:
-        date_time_obj = datetime.datetime.strptime(l_annotation['date'], '%Y-%m-%dT%H:%M:%SZ')
-        l_users_set.add(tuple([l_annotation['user']]))
-        l_projects_set.add(tuple([l_annotation['project_id']]))
-        l_annotations_list.append((
-            l_annotation['user'],
-            l_annotation['project_id'],
-            l_annotation['asset_id'],
-            l_annotation['label'],
-            l_annotation['is_last_checkpoint'],
-            date_time_obj
-        ))
-
-    print(l_users_set)
-    print(l_projects_set)
-    print(l_annotations_list)
-    l_cursor = l_conn.cursor()
-    l_cursor.executemany("insert into users (user) values (?)", list(l_users_set))
-    l_cursor.executemany("insert into projects (project_id) values (?)", list(l_projects_set))
-    l_cursor.executemany("insert into annotations (user, project_id, asset_id, label, is_last_checkpoint, "
-                         "date) values (?, ?, ?, ?, ?, ?)",
-                         l_annotations_list)
-    l_cursor.close()
 
 
 def get_annotations():
     print("get_annotations")
-
-    l_cursor = l_conn.cursor()
-    l_cursor.execute("select a.* from annotations a")
-    l_annotations = l_cursor.fetchall()
-    l_annotations_list = []
-
-    for l_annotation in l_annotations:
-        l_annotations_list.append({
-            "id": l_annotation[0],
-            "user": l_annotation[1],
-            "project_id": l_annotation[2],
-            "asset_id": l_annotation[3],
-            "label": l_annotation[4],
-            "is_last_checkpoint": l_annotation[5],
-            "date": l_annotation[6]
-        })
-
-    l_cursor.close()
-    print(l_annotations_list)
-    return l_annotations_list
+    l_annotations = jsonManager.parse_annotations()
+    print(l_annotations)
+    return l_annotations
 
 
 def get_number_annotations_by_user():
     print("get_number_annotations_by_user")
+    l_annotations = jsonManager.parse_annotations()
+    l_number_annotations_by_user = {}
 
-    l_cursor = l_conn.cursor()
-    l_cursor.execute("select u.user, count(a.id) from users u, annotations a on u.user=a.user group by u.user")
-    l_number_annotations_by_user = l_cursor.fetchall()
-    l_number_by_user_list = []
+    for l_annotation in l_annotations:
+        if l_annotation['user'] not in l_number_annotations_by_user:
+            l_number_annotations_by_user[l_annotation['user']] = 0
+        l_number_annotations_by_user[l_annotation['user']] = l_number_annotations_by_user[l_annotation['user']]+1
 
-    for l_number_by_user in l_number_annotations_by_user:
-        l_number_by_user_list.append({
-            "user": l_number_by_user[0],
-            "number": l_number_by_user[1]
-        })
-
-    l_cursor.close()
-    print(l_number_by_user_list)
-    return l_number_by_user_list
+    print(l_number_annotations_by_user)
+    return l_number_annotations_by_user
 
 
 def get_number_annotations_by_project():
     print("get_number_annotations_by_project")
+    l_annotations = jsonManager.parse_annotations()
+    l_number_annotations_by_project = {}
 
-    l_cursor = l_conn.cursor()
-    l_cursor.execute("select p.project_id, count(a.id) from projects p, annotations a on p.project_id=a.project_id "
-                     "group by p.project_id")
-    l_number_annotations_by_project = l_cursor.fetchall()
-    l_number_by_project_list = []
+    for l_annotation in l_annotations:
+        if l_annotation['project_id'] not in l_number_annotations_by_project:
+            l_number_annotations_by_project[l_annotation['project_id']] = 0
+        l_number_annotations_by_project[l_annotation['project_id']] = l_number_annotations_by_project[l_annotation['project_id']]+1
 
-    for l_number_by_project in l_number_annotations_by_project:
-        l_number_by_project_list.append({
-            "project": l_number_by_project[0],
-            "number": l_number_by_project[1]
-        })
+    print(l_number_annotations_by_project)
+    return l_number_annotations_by_project
 
-    l_cursor.close()
-    print(l_number_by_project_list)
-    return l_number_by_project_list
+
+def get_assets_with_majority_different_labels(_project_id):
+    print("get_assets_with_majority_different_labels for project " + _project_id)
+    l_annotations = jsonManager.parse_annotations()
+    l_labels_by_assets = {}
+
+    for l_annotation in l_annotations:
+        if l_annotation['project_id'] == _project_id:
+            if l_annotation['asset_id'] not in l_labels_by_assets:
+                l_labels_by_assets[l_annotation['asset_id']] = []
+            l_labels_by_assets[l_annotation['asset_id']].append(l_annotation['label'])
+
+    l_result = []
+    for l_tuple in l_labels_by_assets.items():
+        if len(l_tuple[1]) / 2 < len(set(l_tuple[1])):
+            l_result.append(l_tuple[0])
+
+    print(l_result)
 
 
 def initialize():
-    create_tables()
-    insert_data()
+    get_number_annotations_by_user()
+    get_number_annotations_by_project()
+    get_assets_with_majority_different_labels('019mr8mf4a')
